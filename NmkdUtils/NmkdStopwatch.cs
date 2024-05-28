19040ec2eed0710b58e464552831d4289a1c2ff1
N00MKRAD <61149547+n00mkrad@users.noreply.github.com>
2024-05-29 01:09:01 +0200

    Create NmkdStopwatch.cs

---
+++ NmkdUtils/NmkdStopwatch.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NmkdUtils
+{
+    public class NmkdStopwatch : Stopwatch
+    {
+        public long ElapsedMs => ElapsedMilliseconds;
+
+        public NmkdStopwatch(bool startOnCreation = true)
+        {
+            if (startOnCreation)
+                Restart();
+        }
+    }
+}
