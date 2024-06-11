82b314e78bf72c082ed1aa6a41f3ce5d829468b6
N00MKRAD <61149547+n00mkrad@users.noreply.github.com>
2024-06-11 22:35:23 +0200

    Change ffprobe loglevel to avoid interfering with parsing

--- NmkdUtils/FfmpegUtils.cs
+++ NmkdUtils/FfmpegUtils.cs
@@ -9,7 +9,7 @@ namespace NmkdUtils
 
         public static Dictionary<string, string> FfprobeOutputCache = new(); // Key = File hash, Value = Command output
 
-        public static string GetFfprobeOutputCached(string path, string executable = "ffprobe", string args = "-v error -print_format json -show_format -show_streams")
+        public static string GetFfprobeOutputCached(string path, string executable = "ffprobe", string args = "-v panic -print_format json -show_format -show_streams")
         {
             string hash = new FileInfo(path).GetPseudoHash();
 
