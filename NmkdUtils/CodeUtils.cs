7a00528b659278cc7cadb40b0317c645ed4eba22
N00MKRAD <61149547+n00mkrad@users.noreply.github.com>
2024-09-09 01:02:26 +0200

    Add CodeUtils with Assert

---
+++ NmkdUtils/CodeUtils.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace NmkdUtils
+{
+    public class CodeUtils
+    {
+        /// <summary> Checks the failure condition <paramref name="failureCondition"/>. Executes the action <paramref name="failureAction"/> and returns true if the condition is met. </summary>
+        public static bool Assert(Func<bool> failureCondition, Action failureAction = null)
+        {
+            if (failureCondition())
+            {
+                failureAction?.Invoke();
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <inheritdoc cref="Assert(Func{bool}, Action)"/>
+        public static bool Assert(bool failureCondition, Action failureAction = null)
+        {
+            return Assert(() => failureCondition, failureAction);
+        }
+
+    }
+}
