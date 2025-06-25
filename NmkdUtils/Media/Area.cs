5af222c28d79fb76796d678f37405a27db30880b
n00mkrad <61149547+n00mkrad@users.noreply.github.com>
2025-06-25 14:16:52 +0200

    New classes Area (Rectangle with extra stuff) and SizePx (Size with extra stuff for img/vid dimensions)

---
+++ NmkdUtils/Media/Area.cs
@@ -0,0 +1,83 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace NmkdUtils.Media
+{
+    [DebuggerDisplay("X {X} - Y {Y} - {Width}x{Height}")]
+    public class Area
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Point TopLeft => new(X, Y);
+        public Size Size => new(Width, Height);
+
+        public Area() { }
+
+        public Area(int w, int h, int x, int y)
+        {
+            Width = w;
+            Height = h;
+            X = x;
+            Y = y;
+        }
+
+        public static Area FromCropdetect(string cropdetectOutput, int paddingSides = 0, int paddingTopBot = 0, int minHeight = 0)
+        {
+            string crop = cropdetectOutput.SplitIntoLines().Where(l => l.Contains(" crop=") && l.Contains("Parsed_cropdetect")).Last().Split(" crop=").Last();
+            var cropSplit = crop.Split(':').Select(c => c.GetInt()).ToArray();
+            var a = new Area(cropSplit[0], cropSplit[1], cropSplit[2], cropSplit[3]);
+
+            a.ApplyMinHeight(minHeight);
+            a.ApplyPadding(paddingSides, paddingTopBot);
+            return a;
+        }
+
+        // Convert ImageSharp Rectangle to System.Drawing.Rectangle
+        public static Area FromRect(SixLabors.ImageSharp.Rectangle rect, int paddingSides = 0, int paddingTopBot = 0, int minHeight = 0) => 
+            FromRect((new Rectangle(rect.X, rect.Y, rect.Width, rect.Height)), paddingSides, paddingTopBot, minHeight);
+
+        public static Area FromRect(Rectangle rect, int paddingSides = 0, int paddingTopBot = 0, int minHeight = 0)
+        {
+            var a = new Area(rect.Width, rect.Height, rect.X, rect.Y);
+            a.ApplyMinHeight(minHeight);
+            a.ApplyPadding(paddingSides, paddingTopBot);
+            return a;
+        }
+
+        public void ApplyMinHeight(int minHeight)
+        {
+            if (minHeight > 0 && Height < minHeight)
+            {
+                int diff = (minHeight * 2) - Height;
+                Height = minHeight;
+                Y -= (diff / 2f).RoundToInt(MathExtensions.Rounding.Up);
+            }
+        }
+
+        public void ApplyPadding(int paddingSides, int paddingTopBot)
+        {
+            if (paddingSides < 0 || paddingTopBot < 0)
+                return;
+
+            X -= paddingSides;
+            Y -= paddingTopBot;
+            Width += (paddingSides * 2);
+            Height += (paddingTopBot * 2);
+        }
+
+        public string GetCropFilter()
+        {
+            return $"crop={Width}:{Height}:{Math.Max(0, X)}:{Math.Max(0, Y)}";
+        }
+
+        public string GetCropFilterCentered(int totalWidth, int totalHeight)
+        {
+            int x = (totalWidth - Width) / 2;
+            int y = (totalHeight - Height) / 2;
+            return $"crop={Width}:{Height}:{x}:{y}";
+        }
+    }
+}
---
+++ NmkdUtils/Media/SizePx.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace NmkdUtils.Media;
+
+public class SizePx
+{
+    public int Width { get; } = 0;
+    public int Height { get; } = 0;
+    public int AxisSum => Width + Height;
+    public int TotalPx => Width * Height;
+
+    public SizePx() { }
+
+    public SizePx(Size s)
+    {
+        Width = s.Width;
+        Height = s.Height;
+    }
+
+    public SizePx(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+
+    public static implicit operator SizePx(Size s) => new(s); // Implicit cast from System.Drawing.Size to SizePx
+    public static implicit operator Size(SizePx px) => new(px.Width, px.Height); // Implicit cast from SizePx to System.Drawing.Size
+
+    public override string ToString() => $"{Width}x{Height}";
+}
