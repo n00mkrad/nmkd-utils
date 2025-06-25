8a27fe1f9b1ae9f25fc74755e957947a9e10f3e2
n00mkrad <61149547+n00mkrad@users.noreply.github.com>
2025-06-25 14:11:06 +0200

    Some refactoring & cleanup in FormatUtils

--- NmkdUtils/FormatExtensions.cs
+++ NmkdUtils/FormatExtensions.cs
@@ -1,30 +1,35 @@
 ﻿
 
+using System.Diagnostics;
+
 namespace NmkdUtils
 {
     public static class FormatExtensions
     {
-        public static string GetElapsedStr(this System.Diagnostics.Stopwatch sw)
-        {
-            return FormatUtils.Time(sw.Elapsed);
-        }
-
-        public static string Format (this TimeSpan ts)
-        {
-            return FormatUtils.Time(ts);
-        }
-
         /// <summary> Converts a DateTime to a Unix timestamp in whole milliseconds </summary>
         public static long ToUnixMs(this DateTime dt)
         {
             return new DateTimeOffset(dt.ToUniversalTime()).ToUnixTimeMilliseconds();
         }
 
-        public static string AsPercentage(this float value, int decimals = 0)
+        public static string AsPercentage(this float value, int decimals = 0, bool addPercent = true)
         {
             string format = "0." + new string('0', decimals);
-            return (value * 100).ToString(format.Trim('.'));
+            string s = (value * 100).ToString(format.Trim('.'));
+            return addPercent ? s + "%" : s;
         }
 
+        /// <summary> Get formatted time string from a Stopwatch </summary>
+        public static string Format(this Stopwatch sw) => FormatUtils.Time(sw);
+        /// <summary> Get formatted time string from a TimeSpan </summary>
+        public static string Format(this TimeSpan ts) => FormatUtils.Time(ts);
+        /// <summary> Get formatted string from a Size (e.g. "1280x720" unless a different format is specified) </summary>
+        public static string Format(this System.Drawing.Size s, string format = "{0}x{1}") => format.Format(s.Width, s.Height);
+        /// <summary> <inheritdoc cref="Format(System.Drawing.Size, string)"/> </summary>/>
+        public static string Format(this SixLabors.ImageSharp.Size s, string format = "{0}x{1}") => format.Format(s.Width, s.Height);
+        /// <summary> Get formatted string from a byte array </summary>
+        public static string FormatFileSize(this byte[] bytes, bool binaryNotation = true, bool binaryCalculation = false, bool noDecimals = false) => FormatUtils.FileSize(bytes.Length, binaryCalculation, binaryNotation, noDecimals);
+        /// <summary> Get formatted string from a FileInfo </summary>
+        public static string FormatFileSize(this FileInfo fi, bool binaryNotation = true, bool binaryCalculation = false, bool noDecimals = false) => FormatUtils.FileSize(fi.Length, binaryCalculation, binaryNotation, noDecimals);
     }
 }
