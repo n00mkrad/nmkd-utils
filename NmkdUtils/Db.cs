4d154a1ed4b37197b7b649729c50136367060102
n00mkrad <61149547+n00mkrad@users.noreply.github.com>
2025-07-28 15:11:33 +0200

    DB: Minor refactoring & doc

--- NmkdUtils/Db.cs
+++ NmkdUtils/Db.cs
@@ -9,27 +9,31 @@ public class Db
     public static string DbFile => Path.Combine(PathUtils.GetCommonSubdir(PathUtils.CommonDir.Db), Filename);
     private static readonly LiteDatabase _db = new LiteDatabase(DbFile);
 
+    ///<summary> Save the DB - <inheritdoc cref="LiteDatabase.Checkpoint"/> </summary>/>
     public static void Save() => _db?.Checkpoint();
 
+    /// <summary> Gets a key/value <paramref name="collection"/> by name. If no name is provided, the type name is used. </summary>
     public static ILiteCollection<KvEntry<TKey, TVal>> GetKv<TKey, TVal>(object collection) where TKey : notnull
-        => _db.GetCollection<KvEntry<TKey, TVal>>($"{collection}");
+        => Get<KvEntry<TKey, TVal>>(collection);
 
+    /// <summary> Gets a <paramref name="collection"/> by name. If no name is provided, the type name is used. </summary>
     public static ILiteCollection<T> Get<T> (object? collection = null)
     {
         collection ??= typeof(T).Name + "s";
         return _db.GetCollection<T>(collection.ToString());
     }
 
-    public static void Insert<T>(T item, string? collection = null)
-    {
-        collection ??= typeof(T).Name + "s";
-        _db.GetCollection<T>(collection).Insert(item);
-    }
+    /// <inheritdoc cref="ILiteCollection{T}.Insert(T)"/>
+    public static void Insert<T>(T item, string? collection = null) => Get<T>(collection).Insert(item);
 
-    public static void Insert<T>(IEnumerable<T> items, string? collection = null)
+    /// <inheritdoc cref="ILiteCollection{T}.InsertBulk(IEnumerable{T}, int)"/>
+    public static void Insert<T>(IEnumerable<T> items, string? collection = null) => Get<T>(collection).InsertBulk(items);
+
+    /// <summary> Deletes a collection and saves the DB. </summary>
+    public static void DropCollection(string collection)
     {
-        collection ??= typeof(T).Name + "s";
-        _db.GetCollection<T>(collection).InsertBulk(items);
+        _db.DropCollection(collection);
+        Save();
     }
 
     #region Standard Models
